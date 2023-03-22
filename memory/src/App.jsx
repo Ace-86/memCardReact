9e81ff57f4debd2a7efd4549ab777fd89697db19
Ace <kronicrespawn@gmail.com>
2023-03-22 15:55:51 -0500

    styled installed

--- memory/src/App.jsx
+++ memory/src/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import GameBoard from './components/gameboard';
 
 function App() {
   return (
     <p>
-      Cleanedup React App
+      <GameBoard />
     </p>
   );
 }
