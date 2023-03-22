c0458cffd33e93a26293e532db7b011f3aa348fc
Ace <kronicrespawn@gmail.com>
2023-03-22 16:52:28 -0500

    multiple cards added

--- memory/src/components/gameboard.js
+++ memory/src/components/gameboard.js
@@ -1,23 +1,39 @@
 import React from "react";
 import CardTemplate from "./card";
-import Styled from "styled-components";
+
+import GameButton from "./button";
 
 
 export default function GameBoard() {
-
-// const CardContainer = Styled.div`
-// width: 100px;
-// height: 100px;
-// border: 1px solid red;
-// `
-
-
     return (
         <div className="game-board-layout">
-        <CardTemplate 
-        name="Ace of Spades"
-        imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
-        />
+            <div>
+                <CardTemplate 
+                name="Ace of Spades"
+                imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
+                />
+                  <CardTemplate 
+                name="Ace of Spades"
+                imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
+                />
+                  <CardTemplate 
+                name="Ace of Spades"
+                imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
+                />
+                  <CardTemplate 
+                name="Ace of Spades"
+                imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
+                />
+                  <CardTemplate 
+                name="Ace of Spades"
+                imageLink="https://ih1.redbubble.net/image.586275663.5026/flat,750x,075,f-pad,750x1000,f8f8f8.u3.jpg"
+                />
+            </div>
+            <GameButton 
+                title="Play"
+                key= "StartId"
+                round="1"
+            />
         </div>
     )
 }
