1d18919cedba5a616016323075b0b810013905a8
Ace <kronicrespawn@gmail.com>
2023-03-22 16:52:42 -0500

    card style updated

--- memory/src/components/card.js
+++ memory/src/components/card.js
@@ -7,18 +7,22 @@ export default function CardTemplate({name,
 
 
 const CardSquare = Styled.div `
+display: flex;
+align-content: center;
 width: 200px;
 height: 250px;
 background-color: red;
 font-weight: 300;
 font-size: 1rem;
-border: 1px solid red
+border: 1px solid red;
 `
 
 const ImageLink = Styled.img`
+    align-self: center;
     width: 30%;
     height 30%;
 `
+
 const TextWrap = Styled.h1`
     width: 100%;
     height: 100%;
