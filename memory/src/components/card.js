7bb2b1521f261a4ae14ae297418800569b0c04a0
Ace <kronicrespawn@gmail.com>
2023-03-22 15:56:22 -0500

    card component styled

---
+++ memory/src/components/card.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Styled from "styled-components";
+
+///reusable card layout
+export default function CardTemplate({name, 
+    imageLink}) {
+
+
+const CardSquare = Styled.div `
+width: 200px;
+height: 250px;
+background-color: red;
+font-weight: 300;
+font-size: 1rem;
+border: 1px solid red
+`
+
+const ImageLink = Styled.img`
+    width: 30%;
+    height 30%;
+`
+const TextWrap = Styled.h1`
+    width: 100%;
+    height: 100%;
+    color: blue;
+`
+
+    return (
+        <CardSquare className="cardContainer">
+            <ImageLink src= {imageLink} alt={name} />
+            <TextWrap> {name} </TextWrap>
+        </CardSquare>
+    )
+}
