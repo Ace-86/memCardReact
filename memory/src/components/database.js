a776b287b8fa91798ce8168e65c8ad72e73da4cf
Ace <kronicrespawn@gmail.com>
2023-03-23 10:10:15 -0500

    card database created

---
+++ memory/src/components/database.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+
+export const CardDatabase = [
+
+    
+
+    {
+        id: 0,
+        name: "Ace of Spades",
+        image: "random1",
+    },
+    {
+        id: 1,
+        name: "Ace of Hearts",
+        image: "random2"
+    },
+    {
+        id: 2,
+        name: "Ace of Clubs",
+        image: "random3"
+    },
+    {
+        id: 3,
+        name: "Ace of Diamonds",
+        image: "random4"
+    },
+    {
+        id: 4,
+        name: "King of Spades",
+        image: "random5"
+    },
+    {
+        id: 5,
+        name: "King of Hearts",
+        image: "random6"
+    },
+    {
+        id: 6,
+        name: "King of Clubs",
+        image: "random7"
+    },
+    {
+        id: 7,
+        name: "King of Diamonds",
+        image: "random8"
+    },
+    {
+        id: 8,
+        name: "Queen of Spades",
+        image: "random9"
+    },
+    {
+        id: 9,
+        name: "Queen of Hearts",
+        image: "random10"
+    },
+    {
+        id: 10,
+        name: "Queen of Clubs",
+        image: "random11"
+    },
+    {
+        id: 11,
+        name: "Queen of Diamonds",
+        image: "random12"
+    },
+    {
+        id: 12,
+        name: "Jack of Spades",
+        image: "random13",
+    },
+    {
+        id: 13,
+        name: "Jack of Hearts",
+        image: "random14"
+    },
+    {
+        id: 14,
+        name: "Jack of Clubs",
+        image: "random15"
+    },
+    {
+        id: 15,
+        name: "Jack of Diamonds",
+        image: "random16"
+    }
+]
