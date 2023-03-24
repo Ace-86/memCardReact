69cf769117c8adcd6db174d4ec35d94cc96a6ed7
Ace <kronicrespawn@gmail.com>
2023-03-24 16:38:41 -0500

    style updated

--- memory/src/App.jsx
+++ memory/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import './App.css';
 import GameBoard from './components/gameboard';
+import Styled from 'styled-components';
+
+const MainLayout = Styled.div`
+    display: flex;
+    flex-direction: row;
+    background-color: black;
+    min-width: 100%;
+    min-height: 100%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    align-items: center;
+    justify-content: center;
+
+`
 
 function App() {
   return (
-    <div>
+    <MainLayout>
       <GameBoard />
-    </div>
+    </MainLayout>
   );
 }
 
