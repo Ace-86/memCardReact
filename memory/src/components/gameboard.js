33e0ee8e8149d3fe0dd0a3759ccca00ec1192812
Ace <kronicrespawn@gmail.com>
2023-03-24 08:26:12 -0500

    updated style

--- memory/src/components/gameboard.js
+++ memory/src/components/gameboard.js
@@ -6,6 +6,12 @@ import { CardDatabase } from "./database";
 
 const GameLayout = Styled.div`
 display: flex;
+flex-direction: column;
+align-items: center;
+`
+
+const CardLayout = Styled.div`
+display: flex;
 flex-direction: row;
 `
 
@@ -17,14 +23,34 @@ export default function GameBoard() {
 
   return (
         <GameLayout className="game-board-layout">
-            <div>
+            <CardLayout>
                 
                 <CardTemplate 
                 name= {data[0].name}
-                image= "./images/aceclubs.png"
+                image= {data[0].image}
                 />
-             
-            </div>
+
+                <CardTemplate 
+                name= {data[1].name}
+                image= {data[1].image}
+                />
+
+                <CardTemplate 
+                name= {data[5].name}
+                image= {data[5].image}
+                />
+
+                <CardTemplate 
+                name= {data[8].name}
+                image= {data[8].image}
+                />
+
+                <CardTemplate 
+                name= {data[14].name}
+                image= {data[14].image}
+                />
+
+            </CardLayout>
             <GameButton 
                 title="Play"
                 key= "StartId"
