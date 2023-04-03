c39f6ce729495f7c25930c290dd9d3e196ae902a
Ace <kronicrespawn@gmail.com>
2023-04-03 17:51:41 -0500

    style updated

--- memory/src/App.jsx
+++ memory/src/App.jsx
@@ -6,10 +6,10 @@ import Styled from 'styled-components';
 const MainLayout = Styled.div`
     display: flex;
     flex-direction: row;
-    background-color: black;
+    background-color: grey;
     min-width: 100%;
     min-height: 100%;
-    position: fixed;
+    position: absolute;
     top: 0;
     left: 0;
     align-items: center;
