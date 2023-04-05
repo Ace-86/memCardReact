329f45b22fdc394add62e8abfbd616c1b4d81c34
Ace <kronicrespawn@gmail.com>
2023-04-05 12:24:47 -0500

    card is highlighted on click

--- memory/src/components/card.js
+++ memory/src/components/card.js
@@ -1,28 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import Styled from "styled-components";
 
-
 const CardSquare = Styled.div `
-display: flex;
-flex-direction: column;
-width: 200px;
-height: 250px;
-background-color: grey;
-font-weight: 300;
-font-size: 1rem;
-align-items: center;
-justify-items: center;
-padding: 10px;
-gap: 20px;
-margin: 20px;
-border: 2px solid red;
+    display: flex;
+    flex-direction: column;
+    width: 200px;
+    height: 250px;
+    background-color: grey;
+    font-weight: 300;
+    font-size: 1rem;
+    align-items: center;
+    justify-items: center;
+    padding: 10px;
+    gap: 20px;
+    margin: 20px;
+    border: 2px solid red;
+    transition: background-color 0.2s ease;
+    
+    &.selected {
+        background-color: lightblue;
+    }
 `
+
 const ImageLink = Styled.img`
     align-self: center;
     justify-self: center;
     width: 30%;
-    height 30%;
+    height: 30%;
 `
+
 const TextWrap = Styled.h1`
     display: flex;
     flex-direction: row;
@@ -35,15 +41,21 @@ const TextWrap = Styled.h1`
     font-size: 20px;
 `
 
-///reusable card layout
-export default function CardTemplate({name, 
-    image, keyId}) {
-        
-    return (
-        <CardSquare key={keyId} className="cardContainer">
-            <ImageLink src= {image} alt={name} />
-            <TextWrap> {name} </TextWrap>
-        </CardSquare>
-    )
+export default function CardTemplate({name, image, keyId}) {
+    const [isSelected, setIsSelected] = useState(false);
 
+    const handleCardClick = () => {
+        setIsSelected(!isSelected);
+    };
+
+    return (
+        <CardSquare 
+            key={keyId} 
+            className={`cardContainer ${isSelected ? "selected" : ""}`} 
+            onClick={handleCardClick}
+        >
+            <ImageLink src={image} alt={name} />
+            <TextWrap>{name}</TextWrap>
+        </CardSquare>
+    );
 }
