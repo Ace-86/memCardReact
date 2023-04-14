4a77eb37604dfd8e57b9cf0db62a74d74a80ec8e
Ace <kronicrespawn@gmail.com>
2023-04-14 12:46:50 -0500

    removed unused components

--- memory/src/components/Gameboard.js
+++ memory/src/components/Gameboard.js
@@ -1,8 +1,7 @@
 import React from "react";
 import BeginRound from './Gameplay';
 import Styled from "styled-components";
-import GameButton from "./button";
-import { CardDatabase } from "./Database";
+// import GameButton from "./button";
 
 const GameLayout = Styled.div`
 display: flex;
@@ -18,7 +17,6 @@ flex-direction: row;
 export default function GameBoard() {
   //import randomly shuffled card (array) and display 
   //play button should reset rounds, randomize cards again
-  const data = CardDatabase;
 
   return (
         <GameLayout className="game-board-layout">
