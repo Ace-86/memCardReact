8a49ab68a5689698c176996450c21c1c7ee71dd8
Ace <kronicrespawn@gmail.com>
2023-04-14 12:15:50 -0500

    renamed

---
+++ memory/src/components/Database.js
@@ -0,0 +1,86 @@
+import React from "react";
+
+
+export const CardDatabase = [
+
+    {
+        keyId: 0,
+        name: "Ace of Spades",
+        image: "images/acespade.png"
+    },
+    {
+        keyId: 1,
+        name: "Ace of Hearts",
+        image: "images/acehearts.png"
+    },
+    {
+        keyId: 2,
+        name: "Ace of Clubs",
+        image: "images/aceclubs.png"
+    },
+    {
+        keyId: 3,
+        name: "Ace of Diamonds",
+        image: "images/acediamonds.png"
+    },
+    {
+        keyId: 4,
+        name: "King of Spades",
+        image: "images/kingspade.jpeg"
+    },
+    {
+        keyId: 5,
+        name: "King of Hearts",
+        image: "images/kinghearts.jpeg"
+    },
+    {
+        keyId: 6,
+        name: "King of Clubs",
+        image: "images/kingclubs.jpeg"
+    },
+    {
+        keyId: 7,
+        name: "King of Diamonds",
+        image: "images/kingdiamonds.jpeg"
+    },
+    {
+        keyId: 8,
+        name: "Queen of Spades",
+        image: "images/queenspade.jpeg"
+    },
+    {
+        keyId: 9,
+        name: "Queen of Hearts",
+        image: "images/queenhearts.jpeg"
+    },
+    {
+        keyId: 10,
+        name: "Queen of Clubs",
+        image: "images/queenclubs.jpeg"
+    },
+    {
+        keyId: 11,
+        name: "Queen of Diamonds",
+        image: "images/queendiamonds.jpeg"
+    },
+    {
+        keyId: 12,
+        name: "Jack of Spades",
+        image: "images/jackspades.jpeg",
+    },
+    {
+        keyId: 13,
+        name: "Jack of Hearts",
+        image: "images/jackhearts.jpeg"
+    },
+    {
+        keyId: 14,
+        name: "Jack of Clubs",
+        image: "images/jackclubs.jpeg"
+    },
+    {
+        keyId: 15,
+        name: "Jack of Diamonds",
+        image: "images/jackdiamonds.jpeg"
+    }
+]
