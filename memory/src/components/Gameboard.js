fc4f0fa15b082a8a31c86e2c86120888815f4cd7
Ace <kronicrespawn@gmail.com>
2023-04-17 18:01:09 -0500

    removed comments

--- memory/src/components/Gameboard.js
+++ memory/src/components/Gameboard.js
@@ -1,8 +1,7 @@
 import React from "react";
 import BeginRound from './Gameplay';
 import Styled from "styled-components";
-import Score from "./ScoreBoard";
-// import GameButton from "./button";
+
 
 const GameLayout = Styled.div`
 display: flex;
@@ -16,14 +15,10 @@ flex-direction: row;
 `
 
 export default function GameBoard() {
-  //import randomly shuffled card (array) and display 
-  //play button should reset rounds, randomize cards again
-
   return (
         <GameLayout className="game-board-layout">
             <CardLayout>
                 <BeginRound/>
-                {/* <Score /> */}
             </CardLayout>
         </GameLayout>
     )
