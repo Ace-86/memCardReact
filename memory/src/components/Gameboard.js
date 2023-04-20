3f745f6a329f0c46f729dcfa9f98bb1fd1505aaf
Ace <kronicrespawn@gmail.com>
2023-04-20 09:49:57 -0500

    moved Styled to seperate component

--- memory/src/components/Gameboard.js
+++ memory/src/components/Gameboard.js
@@ -1,18 +1,6 @@
 import React from "react";
 import BeginRound from './Gameplay';
-import Styled from "styled-components";
-
-
-const GameLayout = Styled.div`
-display: flex;
-flex-direction: column;
-align-items: center;
-`
-
-const CardLayout = Styled.div`
-display: flex;
-flex-direction: row;
-`
+import { GameLayout, CardLayout} from './Style';
 
 export default function GameBoard() {
   return (
