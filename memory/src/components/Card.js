7db913b9d836975717c0da66d584f2d90d1cba39
Ace <kronicrespawn@gmail.com>
2023-04-20 12:48:06 -0500

    moved styles to seperate component

--- memory/src/components/Card.js
+++ memory/src/components/Card.js
@@ -1,43 +1,5 @@
 import React, { useState } from "react";
-import Styled from "styled-components";
-
-const CardSquare = Styled.div `
-    display: flex;
-    flex-direction: column;
-    width: 200px;
-    height: 250px;
-    font-weight: 300;
-    font-size: 1rem;
-    align-items: center;
-    justify-items: center;
-    padding: 10px;
-    gap: 20px;
-    margin: 20px;
-    transition: box-shadow 0.3s ease-in-out;
-
-    &:hover {
-        box-shadow: 0 0 5px 2px #c7c7c7;
-    }
-   
-`
-
-const ImageLink = Styled.img`
-    align-self: center;
-    justify-self: center;
-    width: 80%;
-    height: 80%;
-`
-
-const TextWrap = Styled.h1`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 20%;
-    color: white;
-    font-size: 20px;
-`
+import { CardSquare, ImageLink, TextWrap } from "./Style";
 
 export default function CardTemplate({name, image, keyId, onSelect}) {
     const [isSelected, setIsSelected] = useState(false);
