bda305e18d778cd5c6abf5c0ad1597f57a3c3d7f
Ace <kronicrespawn@gmail.com>
2023-04-20 09:37:28 -0500

    style updated

--- memory/src/App.jsx
+++ memory/src/App.jsx
@@ -4,15 +4,23 @@ import Board from './components/Gameboard';
 import Styled from 'styled-components';
 import background from './img/felt.jpg'
 
+const Background = Styled.div `
+
+height: 100%;
+
+top: 0;
+left: 0;
+`
 const MainLayout = Styled.div`
     display: flex;
     flex-direction: row;
     background-image: url(${background});
-    min-width: 100%;
-    min-height: 100%;
-    position: absolute;
-    top: 0;
+    width: 100%;
+    height: 100%;
+    backgroud-repeat: no-repeat;
+    position: fixed;
     left: 0;
+    top: 0;
     align-items: center;
     justify-content: center;
 
@@ -20,9 +28,13 @@ const MainLayout = Styled.div`
 
 function App() {
   return (
+    // <Background>
+
     <MainLayout>
       <Board />
     </MainLayout>
+      
+    // </Background>
   );
 }
 
